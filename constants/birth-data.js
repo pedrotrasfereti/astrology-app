3feae6e113fe60bbc95ca40b6d8fd16ce28c0dd4
pedrotrasfereti <pedrotrasfereti@gmail.com>
2025-02-26 20:18:22 -0300

    feat: add birth-data, date and zodiac constants

---
+++ constants/birth-data.js
@@ -0,0 +1,8 @@
+import { MONTHS } from './date.js';
+
+export const YEAR = 2002;
+export const MONTH = MONTHS[5].value;
+export const MONTH_INDEX = MONTHS.findIndex((m) => m.value === MONTH);
+export const DAY = 5;
+export const HOURS = 7;
+export const MINUTES = 25;
---
+++ constants/zodiac.js
@@ -0,0 +1,16 @@
+const ZODIAC_SIGNS = [
+  'Aries',
+  'Taurus',
+  'Gemini',
+  'Cancer',
+  'Leo',
+  'Virgo',
+  'Libra',
+  'Scorpio',
+  'Sagittarius',
+  'Capricorn',
+  'Aquarius',
+  'Pisces'
+];
+
+export default ZODIAC_SIGNS;
